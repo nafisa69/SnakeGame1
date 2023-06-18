67f0ad10c7679f8d2b642989dabfa1665cc55a11
Nafisa <nafisa@gmail.com>
2023-06-19 01:04:57 +0530

    Commit

---
+++ src/Snake_Game.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Snake_Game extends JFrame {
+
+    Board board;
+
+    Snake_Game(){
+        board = new Board();
+        add(board);
+        pack();
+        setResizable(false);
+        setVisible(true);
+
+    }
+    public static void main(String[] args) {
+        // initialize Game object
+        Snake_Game sg= new Snake_Game();
+
+    }
+}
